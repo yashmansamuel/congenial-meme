534097b142929703d9fd50a6aba6db9ef843a4cb
signaturesi.com <yashmansamuel467@gmail.com>
2026-03-24 13:15:50 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,24 +10,20 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      })
+      keys.map(key => key !== CACHE_NAME && caches.delete(key))
     ))
   );
 });
