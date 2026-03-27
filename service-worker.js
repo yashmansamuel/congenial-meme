f0474bc76f5163ac8ae0f84ef2284d3599ec3256
signaturesi.com <yashmansamuel467@gmail.com>
2026-03-27 13:10:20 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'signaturesi-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/favicon.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => key !== CACHE_NAME && caches.delete(key))
+    ))
+  );
+});
